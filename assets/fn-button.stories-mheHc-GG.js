538c279cad751728b6a8615de78ac87435551420
fnhipster <fnhipster@users.noreply.github.com>
2024-12-22 20:41:58 +0000

    deploy: 86ae4d6a0af58f80a6715e009bcdce847f74cb6b

---
+++ assets/fn-button.stories-mheHc-GG.js
@@ -0,0 +1,5 @@
+import{C as m}from"./utils-CG_vfxgm.js";const u={title:"components/fn-button",tags:["autodocs"],parameters:{layout:"centered"},render:({innerHTML:s,...a})=>{const e=m("fn-button",a);return e.innerHTML=s,e},argTypes:{}},r={args:{innerHTML:"Button"}};var t,n,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    innerHTML: 'Button'
+  }
+}`,...(o=(n=r.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const i=["Primary"];export{r as Primary,i as __namedExportsOrder,u as default};
