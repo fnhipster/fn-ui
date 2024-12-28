dfcc66d520363040c06f349bdc5a39520049397a
Carlos A. Cabrera <cacabrer@adobe.com>
2024-12-27 21:14:35 -0600

    today

--- components/fn-timeago/fn-timeago.js
+++ components/fn-timeago/fn-timeago.js
@@ -47,9 +47,7 @@ export default class TimeAgo extends HTMLElement {
     const months = Math.floor(days / 30);
     const years = Math.floor(days / 365);
 
-    if (seconds < 60) return 'just now';
-    if (minutes < 60) return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
-    if (hours < 24) return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
+    if (hours < 24) return 'today';
     if (days < 30) return `${days} day${days !== 1 ? 's' : ''} ago`;
     if (months < 12) return `${months} month${months !== 1 ? 's' : ''} ago`;
     return `${years} year${years !== 1 ? 's' : ''} ago`;
