3666680ccbda3739bb6c061c3b82f8df929e5a33
fnhipster <fnhipster@users.noreply.github.com>
2025-01-02 20:05:29 +0000

    deploy: 773d60707fd22ffec37164b6cd3451dffde37d4d

--- assets/preview-B49rsqkE.js
+++ assets/preview-B49rsqkE.js
@@ -167,7 +167,6 @@ var T=Object.defineProperty;var N=(n,o,t)=>o in n?T(n,o,{enumerable:!0,configura
     ::slotted(a:not(:disabled):not([aria-disabled]):active) {
       color: var(--color-bg);
       background: var(--color-fg);
-      outline: none;
     }
 
     ::slotted(a:not(:disabled):not([aria-disabled]):focus) {
