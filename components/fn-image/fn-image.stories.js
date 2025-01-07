9ff4ab91b5e3653da1d14c4de560ce5ebac2e289
Carlos A. Cabrera <cacabrer@adobe.com>
2025-01-07 13:45:39 -0600

    update(fn-image): modify render function to accept innerHTML and enhance story examples with responsive images

--- components/fn-image/fn-image.stories.js
+++ components/fn-image/fn-image.stories.js
@@ -8,16 +8,10 @@ export default {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  render: ({ ...args }) => {
+  render: ({ innerHTML, ...args }) => {
     const element = CustomElement('fn-image', args);
 
-    const inner = Object.assign(document.createElement('img'), {
-      width: 200,
-      height: 300,
-      src: 'https://picsum.photos/200/300',
-    });
-
-    element.appendChild(inner);
+    element.innerHTML = innerHTML;
 
     return element;
   },
@@ -28,5 +22,21 @@ export default {
 export const Primary = {
   args: {
     poster: 'https://picsum.photos/200/300',
+    innerHTML: `
+      <img loading="lazy" src="https://picsum.photos/200/300" width="200" height="300" alt="Test">
+    `,
+  },
+};
+
+export const EDS = {
+  args: {
+    poster: 'https://main--fnhipster--fnhipster.hlx.page/media_1b4686fe3b45056f4b32885097dca821867370de5.png?width=100&format=webply&optimize=high',
+    innerHTML: `
+    <picture>
+        <source type="image/webp" srcset="https://main--fnhipster--fnhipster.hlx.page/media_1b4686fe3b45056f4b32885097dca821867370de5.png?width=2000&#x26;format=webply&#x26;optimize=medium" media="(min-width: 600px)">
+        <source type="image/webp" srcset="https://main--fnhipster--fnhipster.hlx.page/media_1b4686fe3b45056f4b32885097dca821867370de5.png?width=750&#x26;format=webply&#x26;optimize=medium">
+        <source type="image/png" srcset="https://main--fnhipster--fnhipster.hlx.page/media_1b4686fe3b45056f4b32885097dca821867370de5.png?width=2000&#x26;format=png&#x26;optimize=medium" media="(min-width: 600px)">
+        <img loading="lazy" alt="Image auto generated with Apple Intelligence" src="./media_1b4686fe3b45056f4b32885097dca821867370de5.png?width=750&#x26;format=png&#x26;optimize=medium" width="1024" height="1024">
+    </picture>`.trim(),
   },
 };
