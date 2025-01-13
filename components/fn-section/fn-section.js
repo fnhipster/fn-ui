1367036eb5103c1fdb89b2db1d7de2953944aec5
Carlos A. Cabrera <cacabrer@adobe.com>
2025-01-13 12:47:29 -0600

    section

--- components/fn-section/fn-section.js
+++ components/fn-section/fn-section.js
@@ -3,23 +3,40 @@ const tagName = 'fn-section';
 const template = document.createElement('template');
 
 template.innerHTML = /* html */ `
-  <style>
-    :host {
+  <style>    
+    .wrapper {
+      display: flex;
       max-width: 70rem;
-      align-self: center;
+      align-items: start;
+      justify-self: center;
     }
 
-    :host([layout="centered"]) {
+    :host([layout="centered"]) .wrapper {
       display: flex;
-      justify-self: center;
+      align-items: center;
+      min-height: 60vh;
+      text-align: center;
     }
 
     slot {
       display: block;
+      justify-self: center;
+      align-self: center;
+    }
+
+    .indicator {
+      position: sticky;
+      top: 0;
+      height: 60vh;
+      z-index: -1;
+      width: 1px;
+      margin-left: -1px;
     }
   </style>
 
-  <slot></slot>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
 `;
 
 export default class Section extends HTMLElement {
@@ -29,6 +46,29 @@ export default class Section extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' });
 
     shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        this.dispatchEvent(new CustomEvent('active'));
+      } else {
+        this.dispatchEvent(new CustomEvent('inactive'));
+      }
+    });
+  }, {
+    threshold: 1,
+  });
+
+  connectedCallback() {
+    this.$indicator = document.createElement('div');
+    this.$indicator.classList.add('indicator');
+    this.shadowRoot.querySelector('.wrapper').appendChild(this.$indicator);
+    this.observer.observe(this.$indicator);
+  }
+
+  disconnectedCallback() {
+    this.observer.disconnect();
   }
 }
 
