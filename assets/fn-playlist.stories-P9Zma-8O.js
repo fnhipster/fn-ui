b5b67ba61a77b1d68f2989b16351fe2f68e15529
fnhipster <fnhipster@users.noreply.github.com>
2025-01-21 18:04:59 +0000

    deploy: f50836d32259c5906afd3cadc303373a4e462ca1

---
+++ assets/fn-playlist.stories-P9Zma-8O.js
@@ -0,0 +1,29 @@
+import{C as g}from"./utils-CG_vfxgm.js";const m={title:"components/fn-playlist",tags:["autodocs"],parameters:{layout:"padded"},render:({innerHTML:l,...i})=>{const s=g("fn-playlist",i);return s.innerHTML=l,s},argTypes:{}},o={args:{"apple-music-url":"https://music.apple.com/us/playlist/playlist-name/pl.u-76oNkXoT9G","spotify-url":"https://open.spotify.com/playlist/playlist-id",innerHTML:`
+      <li slot="song"><strong>Song 1</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 2</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 3</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 4</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 5</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 6</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 7</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 8</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 9</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 10</strong> by Some Artist</li>
+    `}};var t,n,r;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    'apple-music-url': 'https://music.apple.com/us/playlist/playlist-name/pl.u-76oNkXoT9G',
+    'spotify-url': 'https://open.spotify.com/playlist/playlist-id',
+    innerHTML: \`
+      <li slot="song"><strong>Song 1</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 2</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 3</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 4</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 5</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 6</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 7</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 8</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 9</strong> by Some Artist</li>
+      <li slot="song"><strong>Song 10</strong> by Some Artist</li>
+    \`
+  }
+}`,...(r=(n=o.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const a=["Primary"];export{o as Primary,a as __namedExportsOrder,m as default};
