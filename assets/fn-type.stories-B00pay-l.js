ada41d5a9002036e2b8988c03589a9e5ac997d4f
fnhipster <fnhipster@users.noreply.github.com>
2025-02-16 03:19:02 +0000

    deploy: e598367d49f4a9538c84e381211157fe091c2e24

---
+++ assets/fn-type.stories-B00pay-l.js
@@ -0,0 +1,17 @@
+import{C as i}from"./utils-CG_vfxgm.js";const p={title:"components/fn-type",tags:["autodocs"],parameters:{layout:"centered"},render:({innerHTML:a,...o})=>{const e=i("fn-type",o);return e.innerHTML=a,e},argTypes:{}},r={args:{innerHTML:`
+      <p>
+        This is a message <br />
+        here is anther line…<br />
+        and <strong>another</strong>.
+      </p>
+    `.trim()}};var n,s,t;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    innerHTML: \`
+      <p>
+        This is a message <br />
+        here is anther line…<br />
+        and <strong>another</strong>.
+      </p>
+    \`.trim()
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const c=["Primary"];export{r as Primary,c as __namedExportsOrder,p as default};
